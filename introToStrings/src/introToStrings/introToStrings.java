f3ec562475f4568bc0e2c95be2456c17bb77d6b1
amelia.brown <amelia.brown@MU-C1-09.mullen.pvt.k12.co.us>
2018-08-30 08:50:29 -0600

    starting challenge

---
+++ introToStrings/src/introToStrings/introToStrings.java
@@ -0,0 +1,29 @@
+package introToStrings;
+import java.util.Scanner;
+public class introToStrings {
+
+	
+	public static void main(String[] args) {
+		challengeOne();
+		challengeTwo();
+	}
+
+	public static void challengeOne()
+	{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Please shout something IN ALL CAPS");
+		String shouting = new userInput.nextline();
+		String sentence = shouting.substring(0,1);
+		System.out.println(sentence);
+		
+	}
+	
+	public static void challengeTwo(){
+		
+	}
+	
+	
+	
+	
+	
+}
